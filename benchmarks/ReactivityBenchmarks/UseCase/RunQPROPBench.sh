80adce930ff17a2b9e8044a8b1223f6353181088
Florian Myter <fmyter@vub.ac.be>
2017-11-25 10:56:17 +0100

    Fixing QPROP max throughput benchmarks
    Signed-off-by: Florian Myter <fmyter@vub.ac.be>

--- benchmarks/ReactivityBenchmarks/UseCase/RunQPROPBench.sh
+++ benchmarks/ReactivityBenchmarks/UseCase/RunQPROPBench.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 for i in {1..30}
 do
-	node MaxThroughput.js --max_old_space_size=12000 true monitor 10000 qprop &
-	node MaxThroughput.js --max_old_space_size=12000 true data 10000 qprop &
-	node MaxThroughput.js --max_old_space_size=12000 true config 10000 qprop &
-	node MaxThroughput.js --max_old_space_size=12000 true driving 10000 qprop &
-	node MaxThroughput.js --max_old_space_size=12000 true geo 10000 qprop &
-	node MaxThroughput.js --max_old_space_size=12000 true dash 10000 qprop
+	node --max_old_space_size=12000 MaxThroughput.js true monitor 10000 qprop &
+	node --max_old_space_size=12000 MaxThroughput.js true data 10000 qprop &
+	node --max_old_space_size=12000 MaxThroughput.js true config 10000 qprop &
+	node --max_old_space_size=12000 MaxThroughput.js true driving 10000 qprop &
+	node --max_old_space_size=12000 MaxThroughput.js true geo 10000 qprop &
+	node --max_old_space_size=12000 MaxThroughput.js true dash 10000 qprop
 done
