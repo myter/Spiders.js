db0c3393b4f110939f93b42c7e6206efedbf2200
Florian Myter <fmyter@vub.ac.be>
2017-11-25 09:57:34 +0100

    wut
    Signed-off-by: Florian Myter <fmyter@vub.ac.be>

--- benchmarks/ReactivityBenchmarks/UseCase/RunQPROPBench.sh
+++ benchmarks/ReactivityBenchmarks/UseCase/RunQPROPBench.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 for i in {1..30}
 do
-	node RunTestCase.js true monitor 10000 qprop &
-	node RunTestCase.js true data 10000 qprop &
-	node RunTestCase.js true config 10000 qprop &
-	node RunTestCase.js true driving 10000 qprop &
-	node RunTestCase.js true geo 10000 qprop &
-	node RunTestCase.js true dash 10000 qprop
+	node RunUseCase.js true monitor 10000 qprop &
+	node RunUseCase.js true data 10000 qprop &
+	node RunUseCase.js true config 10000 qprop &
+	node RunUseCase.js true driving 10000 qprop &
+	node RunUseCase.js true geo 10000 qprop &
+	node RunUseCase.js true dash 10000 qprop
 done
