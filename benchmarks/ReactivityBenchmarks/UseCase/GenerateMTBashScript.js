5864fa41568b6445f7ddb5339205c64816ecffe7
Florian Myter <fmyter@vub.ac.be>
2017-11-25 11:37:20 +0100

    Fixing QPROP max throughput benchmarks
    Signed-off-by: Florian Myter <fmyter@vub.ac.be>

--- benchmarks/ReactivityBenchmarks/UseCase/GenerateMTBashScript.js
+++ benchmarks/ReactivityBenchmarks/UseCase/GenerateMTBashScript.js
@@ -1,26 +1,26 @@
 var fs = require('fs');
 var qpropScript = fs.createWriteStream('RunQPROPMTBench.sh');
 for (var i = 0; i <= 10000; i += 1000) {
-    if (i == 2) {
-        qpropScript.write("for i in {1..30}\n" +
+    if (i == 0) {
+        qpropScript.write("for i in {1..10}\n" +
             "do\n" +
-            "\tnode MaxThroughput.js true monitor " + 2 + " qprop &\n" +
-            "\tnode MaxThroughput.js true data " + 2 + " qprop &\n" +
-            "\tnode MaxThroughput.js true config " + 2 + " qprop &\n" +
-            "\tnode MaxThroughput.js true driving " + 2 + " qprop &\n" +
-            "\tnode MaxThroughput.js true geo " + 2 + " qprop &\n" +
-            "\tnode MaxThroughput.js true dash " + 2 + " qprop\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true monitor " + 2 + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true data " + 2 + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true config " + 2 + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true driving " + 2 + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true geo " + 2 + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true dash " + 2 + " qprop\n" +
             "done\n");
     }
     else {
-        qpropScript.write("for i in {1..30}\n" +
+        qpropScript.write("for i in {1..10}\n" +
             "do\n" +
-            "\tnode MaxThroughput.js true monitor " + i + " qprop &\n" +
-            "\tnode MaxThroughput.js true data " + i + " qprop &\n" +
-            "\tnode MaxThroughput.js true config " + i + " qprop &\n" +
-            "\tnode MaxThroughput.js true driving " + i + " qprop &\n" +
-            "\tnode MaxThroughput.js true geo " + i + " qprop &\n" +
-            "\tnode MaxThroughput.js true dash " + i + " qprop\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true monitor " + i + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true data " + i + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true config " + i + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true driving " + i + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true geo " + i + " qprop &\n" +
+            "\tnode --max_old_space_size=12000 MaxThroughput.js true dash " + i + " qprop\n" +
             "done\n");
     }
 }
