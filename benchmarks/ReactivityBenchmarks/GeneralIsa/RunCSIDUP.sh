a39ebdffc78b50262846f76e2faedfeeca7e5fc0
Florian Myter <fmyter@vub.ac.be>
2017-11-29 17:52:50 +0100

    Fixing QPROP max throughput benchmarks
    Signed-off-by: Florian Myter <fmyter@vub.ac.be>

---
+++ benchmarks/ReactivityBenchmarks/GeneralIsa/RunCSIDUP.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+nodejs Regular.js false pi31 $1 sidupRegular &
+	 nodejs Regular.js false pi32 $1 sidupRegular &
+	 nodejs Regular.js false pi33 $1 sidupRegular &
+	 nodejs Regular.js false pi34 $1 sidupRegular &
+	 nodejs Regular.js false pi35 $1 sidupRegular &
+	 nodejs Regular.js false pi36 $1 sidupRegular &
+	 nodejs Regular.js false pi37 $1 sidupRegular &
+	 nodejs Regular.js false pi38 $1 sidupRegular &
+	 nodejs Regular.js false pi39 $1 sidupRegular &
+	 nodejs Regular.js false pi40 $1 sidupRegular &
+	 nodejs Regular.js false pi41 $1 sidupRegular &
+	 nodejs Regular.js false pi42 $1 sidupRegular &
+	 nodejs Regular.js false pi43 $1 sidupRegular &
+	 nodejs Regular.js false pi44 $1 sidupRegular &
+	 nodejs Regular.js false pi45 $1 sidupRegular
