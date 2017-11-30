c834d2064275b64e53e5b35b2fe5a290bd1c4f26
Florian Myter <fmyter@vub.ac.be>
2017-11-30 11:57:27 +0100

    Fixing QPROP max throughput benchmarks
    Signed-off-by: Florian Myter <fmyter@vub.ac.be>

--- benchmarks/ReactivityBenchmarks/GeneralIsa/Regular.ts
+++ benchmarks/ReactivityBenchmarks/GeneralIsa/Regular.ts
@@ -101,7 +101,7 @@ function getRandomPi(lesserBound,upperbound){
 
 let dynLinks = []
 for(var i=0;i < changes;i++){
-    let fromIndex = getRandomPi(2,59)
+    let fromIndex = getRandomPi(11,59)
     let toIndex   = fromIndex + Math.floor(Math.random() * (59 - fromIndex)) + 1
     let from = eval("pi"+fromIndex)
     let to   = eval("pi"+toIndex)
