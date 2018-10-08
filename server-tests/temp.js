85013d5aa9e6afa7bc240f1c5d1a49769d8562cd
Florian Myter <fmyter@vub.ac.be>
2018-10-08 11:51:46 +0200

    Added express to dependencies

--- server-tests/temp.js
+++ server-tests/temp.js
@@ -2,19 +2,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const spiders_1 = require("../src/spiders");
 var app = new spiders_1.Application();
 class TestActor extends spiders_1.Actor {
-    test() {
-        var waitTill = new Date(new Date().getTime() + 50 * 1000);
-        while (waitTill > new Date()) {
-            let n = (waitTill.getTime() - new Date().getTime()) / 1000;
-            if (!((n - Math.floor(n)) !== 0)) {
-                console.log("Seconds left: " + n);
-            }
-        }
-        console.log("test finished");
+    test(ref) {
+        ref.apply();
     }
 }
 let act = app.spawnActor(TestActor);
-act.test().then((res) => {
+act.test(() => { console.log("Worked!!!!"); }).then((res) => {
     console.log("Promise resolved");
 });
 //# sourceMappingURL=temp.js.map
--- server-tests/temp.ts
+++ server-tests/temp.ts
@@ -13,18 +13,11 @@ import {
 var app = new Application()
 class TestActor extends Actor{
 
-    test(){
-        var waitTill = new Date(new Date().getTime() + 50 * 1000);
-        while(waitTill > new Date()){
-            let n  = (waitTill.getTime() - new Date().getTime()) / 1000
-            if(!((n - Math.floor(n)) !== 0)){
-                console.log("Seconds left: " + n)
-            }
-        }
-        console.log("test finished")
+    test(ref){
+        ref.apply()
     }
 }
 let act = app.spawnActor(TestActor)
-act.test().then((res)=>{
+act.test(()=>{console.log("Worked!!!!")}).then((res)=>{
     console.log("Promise resolved")
 })
